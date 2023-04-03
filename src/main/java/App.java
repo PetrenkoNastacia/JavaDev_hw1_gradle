f61d52c1178adaebc7a2ad083e768a6dca35effe
Anastasiia Petrenko <petrenko.nastacia@gmail.com>
2023-04-03 22:00:55 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import com.google.gson.Gson;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        User user = new User ("Anastasiia", "Petrenko");
+        System.out.println(new Gson().toJson(user));
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private String name;
+    private String lastName;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
